f21544e691b7f00d3f8cef70fb715ba35d8d3990
sumansaurabh <ss.sumansaurabh92@gmail.com>
2024-04-24 12:40:09 +0530

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,23 @@ setup(
     name='netlify-dns-manager',
     version='0.1',
     packages=find_packages(),
+    description="It provides a command line interface to manage DNS records of a domain hosted on Netlify.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     install_requires=[
         'requests',
         'dnspython'
     ],
+    url="https://github.com/sumansaurabh/netlify-dns-manager",
     entry_points={
         'console_scripts': [
             'netlify-dns-manage = netlify_dns_manage.main:main',
         ],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
