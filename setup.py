1ed31e6f917a97e0d0ae0a34f5091f0d401d29ab
sumansaurabh <ss.sumansaurabh92@gmail.com>
2024-04-24 15:11:24 +0530

    fix setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License"
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
