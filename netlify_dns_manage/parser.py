ed3e9c7456c42b5f749b058502ee11b52c582e97
sumansaurabh <ss.sumansaurabh92@gmail.com>
2024-04-25 09:59:32 +0530

    fix the bug for invalid cname value

--- netlify_dns_manage/parser.py
+++ netlify_dns_manage/parser.py
@@ -2,15 +2,22 @@ import zonefile_parser
 from zonefile_parser.record import Record
 
 def parse_zone_file(domain_name, zone_file_path):
-    dns_records = []
+    dns_records = []   
     with open(zone_file_path,"r") as stream:
         content = stream.read()
         records: list[Record] = zonefile_parser.parse(content)
         for record in records:
-            new_name = record.name.replace(f".{domain_name}.","")
+            if record.name == f"{domain_name}.":
+                record.name = "@"
+            elif f".{domain_name}." in record.name:
+                record.name = record.name.replace(f".{domain_name}.","")
+            
+            if 'value' in record.rdata:
+                if (f".{domain_name}..") in record.rdata['value']:
+                    record.rdata['value'] = record.rdata['value'].replace(f".{domain_name}.","")
             modified_record = {
                 "rtype": record.rtype,
-                "name": new_name,
+                "name": record.name,
                 "rclass": record.rclass,
                 "rdata": record.rdata,
                 "ttl": record.ttl
